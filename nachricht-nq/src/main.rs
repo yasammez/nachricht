c337d611fde09586b24cefae7e2e9a1ad1c9617e
Liv <liv.fischer@senacor.com>
2020-12-04 16:40:07 +0100

    pretty printing

--- nachricht-nq/src/main.rs
+++ nachricht-nq/src/main.rs
@@ -5,26 +5,28 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let mut buffer = Vec::new();
     io::stdin().read_to_end(&mut buffer)?;
-    let (field, tail) = Field::decode(&buffer)?;
+    let (field, _) = Field::decode(&buffer)?;
     println!("{}", print_field(&field));
     Ok(())
 }
 
 fn print_field(field: &Field) -> String {
     let name = match field.name {
-        Some(n) => format!("@{}:", n), // TODO: escaping, richtiges Zeichen? & # ' ! ...
+        Some(n) => format!("${}:", n),
         None => "".into(),
     };
     let value = match &field.value {
-        Value::Unit => "null".into(), // TODO: richtiges Wording?
-        Value::Bool(v @ true) => "true".into(),
-        Value::Bool(v @ false) => "false".into(),
+        Value::Unit => "null".into(),
+        Value::Bool(true) => "true".into(),
+        Value::Bool(false) => "false".into(),
         Value::F32(f) => format!("{}", f),
         Value::F64(f) => format!("{}", f),
         Value::Bytes(bytes) => format!("{:02x?}", bytes),
         Value::Int(s, num) => format!("{}{}", match s { Sign::Pos => "+", Sign::Neg => "-" }, num),
         Value::Str(value) => format!("\"{}\"", value), // TODO: escaping
-        Value::Container(fields) => format!("({})", fields.iter().map(|field| print_field(field)).collect::<Vec<String>>().join(",")),
+        Value::Container(fields) => format!("(\n{}\n)", fields.iter()
+            .flat_map(|field| format!("{},", print_field(field)).lines().map(|line| format!("  {}", line)).collect::<Vec<String>>())
+            .collect::<Vec<String>>().join("\n")),
     };
     format!("{}{}", name, value)
 }
