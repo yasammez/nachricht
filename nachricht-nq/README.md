fd3d7bc24f762d8cc8df031e975967dc022df099
Liv <liv.fischer@senacor.com>
2021-02-08 17:41:28 +0100

    Edit files with nq

--- nachricht-nq/README.md
+++ nachricht-nq/README.md
@@ -33,10 +33,18 @@ echo -en "\x7f\x00\x00\x00\x00\x00\x00\x00\x02\x01\x02" | nq -e | hexdump -v -e 
 62 01 02
 ```
 
-Finally, the two switches can be combined to generate the wire format from the textual representation. This is useful to
+The two switches can also be combined to generate the wire format from the textual representation. This is useful to
 quickly feed a nachricht-expecting program some data from the command line.
 
 ```bash
 echo "(true,false)" | nq -te | hexdump -v -e '/1 "%02x "'; echo
 62 01 02
+```
+
+Finally, you can edit any nachricht encoded file with the `-f <PATH>` option. This will open the file in a temporary
+buffer in your default editor to make changes within the textual representation.
+
+```bash
+echo -en "\x62\x01\x02" > nachricht.nch
+nq -f nachricht.nch
 ```
--- nachricht-nq/src/main.rs
+++ nachricht-nq/src/main.rs
@@ -5,6 +5,8 @@ use std::io::{self, Read};
 use anyhow::{Context, Result};
 use structopt::StructOpt;
 use std::str::from_utf8;
+use std::path::PathBuf;
+use std::fs::File;
 
 /// Transform nachricht messages between wire format and textual representation.  By default, input is treated as binary
 /// and output is generated in textual form. This behaviour can be modified by the flags below.
@@ -18,10 +20,31 @@ struct Opt {
     /// Parse the input from the textual representation instead
     #[structopt(short, long)]
     text: bool,
+
+    /// Open a nachricht encoded file in the standard editor
+    #[structopt(short, long, parse(from_os_str))]
+    file: Option<PathBuf>,
 }
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
+    match opt.file {
+        Some(path) => file_mode(path),
+        None => streaming_mode(opt),
+    }
+}
+
+fn file_mode(path: PathBuf) -> Result<()> {
+    let mut buf = Vec::new();
+    File::open(&path)?.read_to_end(&mut buf)?;
+    let field = Decoder::decode(&buf)?.0;
+    let edited = edit::edit(format!("{}", &field))?;
+    let parsed = parser::parse(&edited)?;
+    Encoder::encode(&parsed, &mut File::create(&path)?)?;
+    Ok(())
+}
+
+fn streaming_mode(opt: Opt) -> Result<()> {
     let mut buffer = Vec::new();
     io::stdin().read_to_end(&mut buffer).context("Failed to read stdin")?;
     let field = if opt.text {
